f708441a445d532fdb3f6831a406941e1f13b171
Xiaohan M. <xiaohan.m9@gmail.com>
2025-12-23 23:31:41 -0500

    frontend with map

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Map from './components/Map'
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <header>
+                <h1>SafeJourney - Toronto Safety Router</h1>
+            </header>
+            <Map />
+        </div>
+    )
+}
+
+export default App
